2ac6ac286c8b4122907a99dfa443f8c1472d1591
dasifue <dasifue@gmail.com>
2024-06-26 22:16:52 +0600

    configured database

---
+++ services/database/database.py
@@ -0,0 +1,28 @@
+"Async database connection module"
+
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from .settings import (
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_PORT,
+)
+
+class Base(DeclarativeBase):
+    "class for sqlalchemy ORM system"
+
+
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+engine = create_async_engine(
+    url=DATABASE_URL,
+    echo=True
+)
+session = async_sessionmaker(
+    bind=engine,
+    expire_on_commit=True
+)
---
+++ services/database/settings.py
@@ -0,0 +1,14 @@
+"File for database configuration"
+
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME", "postgres")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
